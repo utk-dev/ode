1ea6ebdec49b76368af3351dc89f24d3c176c596
Utkarsh Gupta <utk1234@outlook.com>
2020-06-06 12:21:24 +0530

    Added Graeffe's method

--- algebraic.py
+++ algebraic.py
@@ -3,6 +3,12 @@ from math import sqrt
 def isEqual(a, b, threshold = 10**(-4)):
     return abs(a - b) < threshold
 
+def poly_apply(poly, x):
+    total = 0
+    for p, coeff in enumerate(list(reversed(poly))):
+        total += coeff*(pow(x, p))
+    return total
+
 def quadratic_solve(a, b, c):
     '''Get both roots of a quadratic equation'''
     D = b*b - 4*a*c
@@ -78,4 +84,41 @@ def lin_bairstow(poly, p, q, steps, api = False, ACCURACY = 4):
             else:
                 print("{0:.{2}f} + ({1:.{2}f})i".format(root.real, root.imag, ACCURACY))
 
+def graeffe(poly, api = False, ACCURACY = 4):
+    ''' Graeffe's Root Squaring Method '''
+    result = [poly]
 
+    # find substituted polynomials
+    for i in range(3):
+        a, b, c, d = poly
+        next_poly = [a**2, 2*a*c-(b**2), (c**2)-2*b*d, -(d**2)]
+        result.append(next_poly)
+        poly = next_poly
+    
+    # find γ₁, γ₂ and γ₃
+    g1 = -poly[1]
+    g2 = -poly[2]/poly[1]
+    g3 = -poly[3]/poly[2]
+
+    # find the roots
+    ra1 = round(pow(g1, 1/8), ACCURACY)
+    ra2 = round(pow(g2, 1/8), ACCURACY)
+    ra3 = round(pow(g3, 1/8), ACCURACY)
+    result.append([round(g1, ACCURACY), round(g2, ACCURACY), round(g3, ACCURACY)])
+    
+    # find sign of the roots
+    r1 = ra1 if abs(poly_apply(result[0], ra1)) < abs(poly_apply(result[0], -ra1)) else -ra1
+    r2 = ra2 if abs(poly_apply(result[0], ra2)) < abs(poly_apply(result[0], -ra2)) else -ra2
+    r3 = ra3 if abs(poly_apply(result[0], ra3)) < abs(poly_apply(result[0], -ra3)) else -ra3
+    result.append([r1, r2, r3])
+
+    if api:
+        return result
+    else:
+        for row, varname in zip(result[:-2], ['x', 'y', 'z', 'u']):
+            print("{0}{4}³ + ({1}){4}² + ({2}){4} + ({3}) = 0".format(*row, varname))
+        print("where x² = y, y² = z and z² = u")
+        print("γ₁: {0:.{3}f}, γ₂: {1:.{3}f}, γ₃: {2:.{3}f}".format(*result[-2], ACCURACY))
+        print("Roots: {0:.{3}f}, {1:.{3}f}, {2:.{3}f}".format(*result[-1], ACCURACY))
+
+graeffe([1, -2, -5, 6])
